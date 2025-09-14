2105507bee8040d9b70c4c1541d17a6f5ed774c1
akshaysharmav7 <akshayshanon@gmail.com>
2025-09-14 20:58:01 +0530

    signup api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -31,12 +31,9 @@ export const signup = async (req, res) => {
             generateToken(newUser._id, res);
             await newUser.save();
 
-            res.status(201).json({ 
-                _id: newUser._id,
-                fullName: newUser.fullName,
-                email: newUser.email,
-                profilePic: newUser.profilePic
-             });
+            res.status(201).json(newUser);
+        }else{
+            res.status(400).json({message:"Invalid User Data"})
         }
 
     } catch (error) {
