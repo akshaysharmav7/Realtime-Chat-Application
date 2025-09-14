77a7285da7688fdbc03b9a40190a256e25f23f4d
akshaysharmav7 <akshayshanon@gmail.com>
2025-09-14 20:29:52 +0530

    first api

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import { connectDb } from "./config/db.mongo.js";
 
 dotenv.config();
 
@@ -12,6 +13,12 @@ const PORT = process.env.PORT;
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+// Express 5 catch all routes 
+app.use((req, res)=>{
+    res.send("We couldn't find the route")
+})
+
 app.listen(PORT, ()=>{
-    console.log(`Server is running at localhost:${PORT}`)
+    console.log(`Server is running at localhost:${PORT}`);
+    connectDb();
 })
