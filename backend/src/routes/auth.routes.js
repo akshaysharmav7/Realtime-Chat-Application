5261373fbae4fb74c7765773e374aad550ae4a76
akshaysharmav7 <akshayshanon@gmail.com>
2025-09-19 09:17:01 +0530

    arcjet protection for rate limiting

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { ProtectedRoute } from "../middlewares/auth.middleware.js";
+import { arcjetProtection } from "../middlewares/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.post("/signup", signup)
+router.use(arcjetProtection)    //middle ware to call this function before the controller in every api
 
+router.post("/signup", signup)
 router.post("/login", login)
 
 router.get("/logout", logout)
@@ -13,5 +15,7 @@ router.get("/logout", logout)
 router.put("/update-profile", ProtectedRoute, updateProfile);
 
 router.get("/check", ProtectedRoute, (req, res) => res.status(200).json(req.user));
+
+router.get("/arcjet-test", arcjetProtection, (req,res)=>{res.send("Testing Arcjet")})
 
 export default router;
